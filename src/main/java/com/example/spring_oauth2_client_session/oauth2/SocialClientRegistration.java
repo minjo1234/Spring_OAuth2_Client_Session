d86801d7534199710d1b0c95784307bdb550d800
Jo Min <122578400+minjo1234@users.noreply.github.com>
2024-05-16 15:25:31 +0900

    first_upload

--- src/main/java/com/example/spring_oauth2_client_session/oauth2/SocialClientRegistration.java
+++ src/main/java/com/example/spring_oauth2_client_session/oauth2/SocialClientRegistration.java
@@ -12,8 +12,8 @@ public class SocialClientRegistration {
     public ClientRegistration naverClientRegistration() {
 
         return ClientRegistration.withRegistrationId("naver")
-                .clientId("FpEs81yrLaMfoElttq2y")
-                .clientSecret("2vhZc7Vv57")
+                .clientId("")
+                .clientSecret("")
                 .redirectUri("http://localhost:8080/login/oauth2/code/naver")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .scope("name", "email")
@@ -27,8 +27,8 @@ public class SocialClientRegistration {
     public ClientRegistration googleClientRegistration() {
 
         return ClientRegistration.withRegistrationId("google")
-                .clientId("858835447109-0842ugb17km53lhi18utng5oar8b86rs.apps.googleusercontent.com")
-                .clientSecret("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")
+                .clientId("")
+                .clientSecret("")
                 .redirectUri("http://localhost:8080/login/oauth2/code/google")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .scope("profile", "email")
@@ -44,8 +44,8 @@ public class SocialClientRegistration {
     public ClientRegistration githubClientRegistration() {
 
         return ClientRegistration.withRegistrationId("github")
-                .clientId("Ov23liTvpcPvgZl0DYNo")
-                .clientSecret("2e59dd82334d6871e7bee03254712e9588e408a4")
+                .clientId("")
+                .clientSecret("")
                 .redirectUri("http://localhost:8080/login/oauth2/code/github")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .scope("name", "email")
@@ -58,8 +58,8 @@ public class SocialClientRegistration {
     public ClientRegistration facebookClientRegistration() {
 
         return ClientRegistration.withRegistrationId("facebook")
-                .clientId("799792711787725")
-                .clientSecret("e17b2a0c9855779c7a44db1064b6452e")
+                .clientId("")
+                .clientSecret("")
                 .redirectUri("http://localhost:8080/login/oauth2/code/facebook")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .scope("public_profile" , "email")
